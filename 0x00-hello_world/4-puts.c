1032f12b2f133906ea1b53598aa6803416e92c8e
Photolo <photolo.gift@gmail.com>
2022-09-12 13:24:43 -0700

    puts.c: Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
 
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
 
 
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+int main(void)
+{
+ puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
