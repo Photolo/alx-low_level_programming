e6cb2ccbb5727388ee856569055b5813315ed038
Photolo <photolo.gift@gmail.com>
2022-12-09 06:31:25 -0800

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,38 +4,9 @@
 * main - print the string in the put function
 *
 * Description: using the main function
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 */
 int main(void)
 {
- puts("Programming is like building a multilingual puzzle\n");
-return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
