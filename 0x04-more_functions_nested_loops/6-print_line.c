22575ad16dae35801edf70b27c21c60009a2047f
Photolo <photolo.gift@gmail.com>
2022-12-16 13:12:31 -0800

    Task6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,16 +2,17 @@
 
 /**
  * print_line - prints line
+ *
  * @n: param
+ * Return: No return.
  */
-
 void print_line(int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar('_');
+		_putchar(95);
 	}
 	_putchar('\n');
 }
