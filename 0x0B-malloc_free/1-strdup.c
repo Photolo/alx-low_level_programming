479d99c14badabe3325a0827bf4765201fc02d21
Photolo <photolo.gift@gmail.com>
2023-01-11 03:15:52 -0800

    The fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,19 +10,31 @@
 
 char *_strdup(char *str)
 {
+	char *aaa;
+
+	int i, r = 0;
+
+
 	 if (str == NULL)
-	 {
+	 
 		 return (NULL);
-	 }
+	 
+	 i = 0;
 
-	 size_t len = strlen(str);
-	 char *result = malloc(len + 1);
-	  if (result == NULL)
-	  {
-		  return (NULL);
-	  }
+	 while (str[i] != '\0')
 
-	  strcpy(result, str);
-	    return (result);
+		 i++;
+
+	 aaa = malloc(sizeof(char) * (i + 1));
+
+	 if (aaa == NULL)
+		 
+		 return (NULL);
+
+	 for (r = 0; str[r]; r++)
+
+		 aaa[r] = str[r];
+
+	 return (aaa);
+
 }
-
